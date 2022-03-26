82f0475f582af9fde07795475a90f191daa63513
CristiCode <cristicode12@gmail.com>
2022-03-26 19:07:04 +0100

    Refactorizacion y generacion de Javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,8 +1,15 @@
 package cuentas;
 
+/**
+ * Clase CCuenta Define los objetos necesarios para la representacion de una
+ * cuenta bancaria
+ *
+ */
 public class CCuenta {
 
     /**
+     * Devuelve valor de la variable nombre
+     *
      * @return the nombre
      */
     public String getNombre() {
@@ -10,13 +17,18 @@ public class CCuenta {
     }
 
     /**
-     * @param nombre the nombre to se t
+     * Recibe un parametro de tipo String para cambiar el valor de la varibale
+     * nombre
+     *
+     * @param nombre the nombre to set
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
+     * Devuelve el valor de la variable cuenta
+     *
      * @return the cuenta
      */
     public String getCuenta() {
@@ -24,6 +36,9 @@ public class CCuenta {
     }
 
     /**
+     * Recibe un parametro de tipo String para cambiar el valor de la varibale
+     * cuenta
+     *
      * @param cuenta the cuenta to set
      */
     public void setCuenta(String cuenta) {
@@ -31,6 +46,8 @@ public class CCuenta {
     }
 
     /**
+     * Devuelve el valor de la variable radio
+     *
      * @return the saldo
      */
     public double getSaldo() {
@@ -38,6 +55,9 @@ public class CCuenta {
     }
 
     /**
+     * Recibe un parametro de tipo double para cambiar el valor de la varibale
+     * saldo
+     *
      * @param saldo the saldo to set
      */
     public void setSaldo(double saldo) {
@@ -45,6 +65,8 @@ public class CCuenta {
     }
 
     /**
+     * Devuelve el valor de la variable tipoInteres
+     *
      * @return the tipoInterés
      */
     public double getTipoInterés() {
@@ -52,47 +74,73 @@ public class CCuenta {
     }
 
     /**
+     * Recibe un parametro de tipo double para cambiar el valor de la varibale
+     * tipoInteres
+     *
      * @param tipoInterés the tipoInterés to set
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
-
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    /**
+     * Constructor CCuenta vacio.
+     * Genera un objeto sin parametros
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Constructor CCuenta con parametros
+     * Genera un objeto que recibe parametros de tipo String y double
+     * @param nom
+     * @param sal
+     * @param cue
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    /**
+     * Metodo que devuelve valor obtenido del metodo getSaldo()
+     * @return devuelve el saldo de la cuenta
+     */
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Metodo que recibe por parametro una variable de tipo double
+     * @param cantidad
+     * @throws java.lang.Exception
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * Metodo que recibe por parametro una variable de tipo double
+     * @param cantidad
+     * @throws java.lang.Exception
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 }
